b9bd1d5d8c22328c897b4c33f32ac5675244cd5a
beyza Bolattekin <beyza@beyzas-MacBook-Air.local>
2025-03-06 21:34:53 +0300

    basic to do events

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <!-- composition API version -->
+<!-- splice() metodu ile: 
+silme → splice(index, 1)
+ekleme → splice(index, 0, newItem)
+değiştirme → splice(index, 1, newItem) -->
+
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 const name = ref("john doe");
 const status = ref("active");
 const tasks = ref(["task one", "task two", "task three"]);
+const newTask = ref("");
 
 const toggleStatus = () => {
   if (status.value === "active") {
@@ -14,6 +20,27 @@ const toggleStatus = () => {
     status.value = "active";
   }
 };
+
+const addTask = () => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+
+onMounted(async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+    tasks.value = data.map((task) => task.title);
+  } catch (error) {
+    console.log("Error fetching tasks");
+  }
+});
 </script>
 
 <template>
@@ -22,13 +49,27 @@ const toggleStatus = () => {
   <p v-else-if="status === 'pending'">User id pending</p>
   <p v-else>User is inactive</p>
 
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input
+      type="text"
+      id="newTask"
+      name="newTask"
+      v-model="newTask"
+    />
+    <button type="submit">Submit</button>
+  </form>
+
   <h3>Tasks:</h3>
   <ul>
     <li
-      v-for="task in tasks"
+      v-for="(task, index) in tasks"
       :key="task"
     >
-      {{ task }}
+      <span>
+        {{ task }}
+      </span>
+      <button @click="deleteTask(index)">x</button>
     </li>
   </ul>
   <!-- <button v-on:click="toggleStatus">Change Status</button> -->
